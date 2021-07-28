9fc257e78850cb7b2e92cafe503168dfebbc625c
mador-till-prod <61970359+mador-till-prod@users.noreply.github.com>
2021-07-28 12:53:30 +0300

    Update util.js

--- code/util.js
+++ code/util.js
@@ -1,1 +1,54 @@
-var _0x232b=["\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x66\x69\x65\x6C\x64\x73","\x61\x73\x73\x69\x67\x6E","\x63\x6C\x61\x73\x73\x65\x73","\x6C\x65\x6E\x67\x74\x68","\x61\x64\x64","\x63\x6C\x61\x73\x73\x4C\x69\x73\x74","\x63\x6C\x73","\x69\x64","\x66\x69\x6C\x74\x65\x72","\x61\x70\x70\x65\x6E\x64","\x6C\x69\x73\x74\x65\x6E\x65\x72\x73","\x6B\x65\x79\x73","\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72","\x61\x74\x74\x72\x69\x62\x75\x74\x65\x73","\x73\x65\x74\x41\x74\x74\x72\x69\x62\x75\x74\x65","\x2C","\x73\x70\x6C\x69\x74","\x74\x72\x61\x6E\x73\x69\x74\x69\x6F\x6E\x50\x72\x6F\x70\x65\x72\x74\x79","\x62\x69\x6E\x64","\x69\x6E\x64\x65\x78\x4F\x66","\x6D\x61\x70","\x74\x72\x61\x6E\x73\x69\x74\x69\x6F\x6E\x44\x75\x72\x61\x74\x69\x6F\x6E","\x74\x72\x61\x6E\x73\x69\x74\x69\x6F\x6E\x44\x65\x6C\x61\x79","\x73\x74\x72\x69\x6E\x67","\x2D","\x74\x6F\x4C\x6F\x77\x65\x72\x43\x61\x73\x65","","\x72\x65\x70\x6C\x61\x63\x65","\x74\x72\x69\x6D"];function El(_0x2838x2,_0x2838x3= {},..._0x2838x4){let _0x2838x5=Object[_0x232b[2]](document[_0x232b[0]](_0x2838x2),_0x2838x3[_0x232b[1]]|| {});if(_0x2838x3[_0x232b[3]]&& _0x2838x3[_0x232b[3]][_0x232b[4]]){_0x2838x5[_0x232b[6]][_0x232b[5]](..._0x2838x3[_0x232b[3]])}else {if(_0x2838x3[_0x232b[7]]){_0x2838x5[_0x232b[6]][_0x232b[5]](_0x2838x3[_0x232b[7]])}};if(_0x2838x3[_0x232b[8]]){_0x2838x5[_0x232b[8]]= _0x2838x3[_0x232b[8]]};_0x2838x5[_0x232b[10]](..._0x2838x4[_0x232b[9]]((_0x2838x5)=>{return _0x2838x5}));for(let _0x2838x6 of Object[_0x232b[12]](_0x2838x3[_0x232b[11]]|| {})){if(_0x2838x3[_0x232b[11]][_0x2838x6]){_0x2838x5[_0x232b[13]](_0x2838x6,_0x2838x3[_0x232b[11]][_0x2838x6],false)}};for(let _0x2838x7 of Object[_0x232b[12]](_0x2838x3[_0x232b[14]]|| {})){if(_0x2838x3[_0x232b[14]][_0x2838x7]!== undefined){_0x2838x5[_0x232b[15]](_0x2838x7,_0x2838x3[_0x232b[14]][_0x2838x7])}};return _0x2838x5}function timeToTransition(_0x2838x5,_0x2838x9,_0x2838xa= getComputedStyle(el)){if(_0x2838x9 instanceof  Array){if(_0x2838x9[_0x232b[4]]=== 1){return [timeToTransition(_0x2838x5,_0x2838x9[0])]};let _0x2838xb=_0x2838xa[_0x232b[18]][_0x232b[17]](_0x232b[16]);let _0x2838xc=_0x2838x9[_0x232b[21]](_0x2838xb[_0x232b[20]][_0x232b[19]](_0x2838xb));let _0x2838xd=_0x2838xa[_0x232b[22]][_0x232b[17]](_0x232b[16]);let _0x2838xe=_0x2838xa[_0x232b[23]][_0x232b[17]](_0x232b[16]);return _0x2838xc[_0x232b[21]]((_0x2838xf)=>{return (parseFloat(_0x2838xd[_0x2838xf])+ parseFloat(_0x2838xe[_0x2838xf]))* 1000})};if( typeof _0x2838x9=== _0x232b[24]){_0x2838x9= _0x2838x9[_0x232b[28]](/[A-Z]/,(_0x2838x10)=>{return `${_0x232b[25]}${_0x2838x10[_0x232b[26]]()}${_0x232b[27]}`});let _0x2838xd=_0x2838xa[_0x232b[22]][_0x232b[17]](_0x232b[16]);let _0x2838xe=_0x2838xa[_0x232b[23]][_0x232b[17]](_0x232b[16]);let _0x2838x11=_0x2838xa[_0x232b[18]][_0x232b[29]]()[_0x232b[17]](/ *, */)[_0x232b[20]](_0x2838x9);return _0x2838x11===  -1?0:(parseFloat(_0x2838xd[_0x2838x11]|| _0x2838xd[0])+ parseFloat(_0x2838xe[_0x2838x11]|| _0x2838xe[0]))* 1000};return (parseFloat(_0x2838xa[_0x232b[22]])+ parseFloat(_0x2838xa[_0x232b[23]]))* 1000}
+// type Listeners = { [M in keyof (HTMLElementEventMap & SVGSVGElementEventMap)]?: (this: HTMLElement, e: (HTMLElementEventMap & SVGSVGElementEventMap)[M]) => void }
+/**
+ * @type {{ [M in keyof (HTMLElementEventMap & SVGSVGElementEventMap)]?: (this: HTMLElement, e: (HTMLElementEventMap & SVGSVGElementEventMap)[M]) => void }} Listeners
+ */
+
+// export function El<T>(tagName: string, options?: {
+//     classes?: string[], cls?: string,
+//     id?: string, fields?: T, attributes?: { [index: string]: string | { toString(): string } }, listeners?: Listeners
+// }, ...children: (string | Node)[]): T & HTMLElement;
+/**
+ * @template T
+ * @param {keyof HTMLElementTagNameMap} tagName 
+ * @param {{classes?: string[], cls?: string, id?: string, attributes: {[index: string]: string | { toString(): string }}, listeners: Listeners}} options 
+ * @param  {...string | Node} children 
+ */
+function El(tagName, options = {}, ...children) {
+    let el = Object.assign(document.createElement(tagName), options.fields || {});
+    if (options.classes && options.classes.length) el.classList.add(...options.classes);
+    else if (options.cls) el.classList.add(options.cls);
+    if (options.id) el.id = options.id;
+    el.append(...children.filter(el => el));
+    for (let listenerName of Object.keys(options.listeners || {}))
+        if (options.listeners[listenerName]) el.addEventListener(listenerName, options.listeners[listenerName], false);
+    for (let attributeName of Object.keys(options.attributes || {})) {
+        if (options.attributes[attributeName] !== undefined) el.setAttribute(attributeName, options.attributes[attributeName]);
+    }
+    return el;
+}
+
+/**
+ * 
+ * @param {Element} el 
+ * @param {string | string[]} [prop] 
+ * @param {CSSStyleDeclaration} style 
+ * @returns {number}
+ */
+function timeToTransition(el, prop, style = getComputedStyle(el)) {
+    if (prop instanceof Array) {
+        if (prop.length === 1) return [timeToTransition(el, prop[0])];
+        let props = style.transitionProperty.split(",");
+        let indexArr = prop.map(props.indexOf.bind(props));
+        let durs = style.transitionDuration.split(",");
+        let delays = style.transitionDelay.split(",");
+        return indexArr.map((i) => (parseFloat(durs[i]) + parseFloat(delays[i])) * 1000);
+    }
+    if (typeof prop === "string") {
+        prop = prop.replace(/[A-Z]/, (c) => `-${c.toLowerCase()}`);
+        let durs = style.transitionDuration.split(",");
+        let delays = style.transitionDelay.split(",");
+        let index = style.transitionProperty.trim().split(/ *, */).indexOf(prop);
+        return index === -1 ? 0 : (parseFloat(durs[index] || durs[0]) + parseFloat(delays[index] || delays[0])) * 1000
+    }
+    return (parseFloat(style.transitionDuration) + parseFloat(style.transitionDelay)) * 1000;
+}
